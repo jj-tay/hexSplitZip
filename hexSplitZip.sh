a1d0596923ae37e9467ae472054a11fdd1f13371
Tay Jun Jie <jjat1987@gmail.com>
2022-02-24 01:19:24 +0800

    Correct zip command

--- hexSplitZip.sh
+++ hexSplitZip.sh
@@ -3,6 +3,5 @@ set -e
 xxd $1 | split -b 1GB -d --additional-suffix ".txt" - "$1."
 for HEXFILE in $1.*.txt;
     do
-        zip -9 $HEXFILE.zip $HEXFILE
-        rm $HEXFILE
+        zip -m9 $HEXFILE.zip $HEXFILE
 done
